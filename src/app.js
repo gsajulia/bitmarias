4c5a0cf8f412197dc03e0ff9fd9bd71424912e80
julia <juliagabriela98@gmail.com>
2019-10-24 20:53:01 -0300

    using ejs / body-parser now, POST method is working in parts but is unfinished.

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 //to use ejs create a folder named views, add a new file "index.ejs" and put the content of the index.html there
-let express = require('express');
-
+const express = require('express');
+const bodyParser = require('body-parser');
 let app = express();
 
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 app.use('/images', express.static('images'));
 
-// app.get('/', function (req, res) {
-//     res.render('index') ;
-// }); //this form is for ejs
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html') ;
+app.get('/',  function (req, res) {
+    //res.header("Content-Type", "text/html; charset=utf-8");
+    res.render('index', {qs: req.query}) ;
+}); //this form is for ejs
+app.post('/', urlencodedParser, function (req, res) {
+    //res.header("Content-Type", "text/html; charset=utf-8");
+    console.log(req.body);
+    res.render('contact-sucess', {data: req.body}) ;
 });
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/index.html') ;
+// });
 // app.get('/profile/:name', function (req, res) {
 //     let data = {age: 23, job:'ninja', hobbies:['eating', 'fishing']};
 //     res.render('profile', {person: req.params.name, data: data});
